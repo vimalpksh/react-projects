eb132c2536598ed9908b57fc8c10ed6433cdc811
vimalpksh <itisvimal@gmail.com>
2023-11-13 01:05:59 +0530

    basic review component done

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -1,9 +1,46 @@
-import React, { useState } from 'react';
-import people from './data';
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import React, { useState } from "react";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 const Review = () => {
-  return <h2>review component</h2>;
-};
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
+  const checkNumber = number;
 
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return newIndex;
+    });
+
+    const prevPerson = () => {
+      setIndex((index) => {
+        let newIndex = index - 1;
+        return newIndex;
+      });
+    };
+    return (
+      <article className="review">
+        <div className="img-container">
+          <img src={image} alt={name} className="person-img" />
+          <span className="quote-icon">
+            <FaQuoteRight />
+          </span>
+        </div>
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
+        <div className="button-container">
+          <button className="prev-btn" onClick={prevPerson}>
+            <FaChevronLeft />
+          </button>
+          <button className="next-btn" onClick={nextPerson}>
+            <FaChevronRight />
+          </button>
+        </div>
+        <button className="random-btn">Suprise me</button>
+      </article>
+    );
+  };
+};
 export default Review;
